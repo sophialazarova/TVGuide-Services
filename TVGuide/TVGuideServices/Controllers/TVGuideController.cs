f33992e8ff06d77716431abc1c9f7258aae05dec
Sophia Lazarova <sophia.lazarova@yahoo.com>
2015-01-03 14:38:24 +0200

    some fixes

--- TVGuide/TVGuideServices/Controllers/TVGuideController.cs
+++ TVGuide/TVGuideServices/Controllers/TVGuideController.cs
@@ -17,14 +17,10 @@
         public async Task<IHttpActionResult> GetScheduleForChannel(int channel, string date)
         {
             const string COMMON_URL = "http://www.start.bg/lenta/tv-programa//tv/show/channel/";
-            const int TIME_LENGTH = 5;
-            const int TIME_START_INDEX = 0;
-            const int NAME_START_INDEX = 19;
-
             var urlForChannel = COMMON_URL + channel + "/" + date + "/" + "0";
 
             var client = new HttpClient();
-            var request= new HttpRequestMessage();
+            var request = new HttpRequestMessage();
             request.RequestUri = new Uri(urlForChannel);
             request.Method = HttpMethod.Get;
 
@@ -35,17 +31,21 @@
                 var html = reader.ReadToEnd();
                 var htmlDoc = new HtmlDocument();
                 htmlDoc.LoadHtml(html);
-                var hours = htmlDoc.DocumentNode.SelectSingleNode("//ul[@class=\"tv-dlist\"]");
-                
-                var groups = hours.SelectNodes("li");
-                var res = groups.Select(node => node.InnerText.Trim());
+                var schedule = htmlDoc.DocumentNode.SelectSingleNode("//ul[@class=\"tv-dlist\"]");
+
+                var groups = schedule.SelectNodes("li");
+                var tvEntry = groups.Select(node => node.InnerText.Trim()).ToList();
                 var result = new List<TVProgramEntry>();
-                foreach(var line in res){
-                    var time = line.Substring(TIME_START_INDEX, TIME_LENGTH);
-                    var name = line.Substring(NAME_START_INDEX);
-                    TVProgramEntry entry = new TVProgramEntry(name,time);
+                for (int i = 0; i < tvEntry.Count; i++)
+                {
+                    var line = tvEntry[i];
+                    var timeAndTitle = line.Split(new string[] { "  " }, StringSplitOptions.RemoveEmptyEntries);
+                    string time= timeAndTitle[0].Trim();
+                    string title = timeAndTitle[1];
+                    TVProgramEntry entry = new TVProgramEntry(title, time);
                     result.Add(entry);
                 }
+
                 return Ok(result);
             }
         }
@@ -61,35 +61,36 @@
             request.RequestUri = new Uri(url);
             request.Method = HttpMethod.Get;
             var response = await client.SendAsync(request);
-             var responseStream = await response.Content.ReadAsStreamAsync();
-             using (StreamReader reader = new StreamReader(responseStream))
-             {
-                 var html = reader.ReadToEnd();
-                 var htmlDoc = new HtmlDocument();
-                 htmlDoc.LoadHtml(html);
-                 var channels = htmlDoc.DocumentNode.SelectNodes("//div[@class=\"channel\"]");
-                 var series = htmlDoc.DocumentNode.SelectNodes("//ul[@class=\"tv-dlist search\"]").ToList();
-                 var resChannels = channels.Select(node => node.InnerText).ToList();
-                 var dict = new List<TVChannelScheduleForSeries>();
-                 for (int i = 0; i < series.Count; i++)
-                 {
-                     var entry = series[i];
-                     var info = entry.SelectNodes("li").ToList();
-                     var seriesOnChannel = new List<TVSeriesEntry>();
-                     for (int j = 0; j < info.Count; j++)
-                     {
-                         var li = info[j];
-                         var day = li.SelectSingleNode("div[@class=\"day\"]").InnerText;
-                         var name = li.SelectSingleNode("div[@class=\"title\"]").InnerText.Replace("\r", string.Empty);
-                         var time = li.SelectSingleNode("div[@class=\"time\"]").InnerText;
-                         TVSeriesEntry s = new TVSeriesEntry(name, time, day);
-                         seriesOnChannel.Add(s);
-                     }
-                     dict.Add(new TVChannelScheduleForSeries(resChannels[i], seriesOnChannel));
-                 }
-                 return Ok(dict);
+            var responseStream = await response.Content.ReadAsStreamAsync();
+            using (StreamReader reader = new StreamReader(responseStream))
+            {
+                var html = reader.ReadToEnd();
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(html);
+                var channels = htmlDoc.DocumentNode.SelectNodes("//div[@class=\"channel\"]");
+                var series = htmlDoc.DocumentNode.SelectNodes("//ul[@class=\"tv-dlist search\"]").Where(node => node.InnerText.Trim() != string.Empty).ToList();
+                var resChannels = channels.Select(node => node.InnerText).ToList();
+                var dict = new List<TVChannelScheduleForSeries>();
+                for (int i = 0; i < series.Count; i++)
+                {
+                    var entry = series[i];
+                    var info = entry.SelectNodes("li");
+                    var seriesOnChannel = new List<TVSeriesEntry>();
+                    for (int j = 0; j < info.Count; j++)
+                    {
+                        var li = info[j];
+                        var day = li.SelectSingleNode("div[@class=\"day\"]").InnerText;
+                        var name = li.SelectSingleNode("div[@class=\"title\"]").InnerText.Replace("\r", string.Empty);
+                        var time = li.SelectSingleNode("div[@class=\"time\"]").InnerText;
+                        TVSeriesEntry s = new TVSeriesEntry(name, time, day);
+                        seriesOnChannel.Add(s);
+                    }
 
-             }
+                    dict.Add(new TVChannelScheduleForSeries(resChannels[i], seriesOnChannel));
+                }
+
+                return Ok(dict);
+            }
         }
 
     }
